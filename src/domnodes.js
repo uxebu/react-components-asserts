0986cb31686d63cd2c440f63484ebb748afeb22d
Wolfram Kriesing <wk@uxebu.com>
2015-07-16 19:38:56 +0200

    Move all the rendering into Renderer.

--- src/domnodes.js
+++ src/domnodes.js
@@ -1,40 +1,17 @@
 import DomNode from './domnode.js';
 import ReactComponent from './reactcomponent.js';
+import Renderer from './renderer.js';
 
 export default class DomNodes {
-  static fromRenderedTree(tree) {
+  
+  static fromComponent(component) {
+    const renderedTree = Renderer.withComponent(component).allDomNodes();
     let instance = new DomNodes();
-    instance.domNodes = allNodes(tree)
-      .filter(DomNode.isDomNode)
+    instance.domNodes = allNodes(renderedTree)
       .map(domNode => DomNode.fromRenderedNode(domNode));
     return instance;
   }
   
-  static fromComponent(component) {
-    return DomNodes.fromRenderedTree(renderRecursively(component));
-  }
-}
-
-function renderRecursively(componentToRender) {
-  const rendered = render(componentToRender);
-  if (rendered && rendered.props  && rendered.props.children){
-    if (Array.isArray(rendered.props.children)) {
-      rendered.props.children = rendered.props.children.map(renderRecursively);
-    } else {
-      rendered.props.children = [rendered.props.children].map(renderRecursively)[0];
-    }
-  }
-  return rendered;
-}
-
-function render(componentToRender) {
-  if (DomNode.isDomNode(componentToRender)) {
-    return componentToRender;
-  }
-  if (ReactComponent.isReactComponent(componentToRender)) {
-    return ReactComponent.render(componentToRender);
-  }
-  return componentToRender;
 }
 
 const ensureToBeArray = (mayBeArray) => Array.isArray(mayBeArray) ? mayBeArray : [mayBeArray];
@@ -55,17 +32,3 @@ function allChildren({props = {}}) {
 function allNodes(tree) {
   return [tree, ...allChildren(tree)];
 }
-
-
-
-//rendersDomNodeAttributeWithValue
-//rendersDomNodeWithInnerText
-
-//return (
-//  <div className="email-item pure-g">
-//    <a className="pure-u-3-4" href={newUrl}>
-//      <h4 className="email-subject">{name}</h4>
-//      <p className="email-desc">{description}</p>
-//    </a>
-//  </div>
-//);
--- src/renderer.js
+++ src/renderer.js
@@ -2,6 +2,11 @@ import DomNode from './domnode.js';
 import ReactComponent from './reactcomponent.js';
 
 export default class Renderer {
+  
+  constructor() {
+    this._rendered = false;
+  }
+  
   static withComponent(component) {
     let renderer = new Renderer();
     renderer.component = component;
@@ -9,36 +14,46 @@ export default class Renderer {
   }
   
   allReactComponents() {
-    return allReactComponents(this.component);
-  }
-}
-
-
-function allReactComponents(componentToRender) {
-  function renderRecursively(componentToRender) {
-    const rendered = render(componentToRender);
-    if (rendered && rendered.props  && rendered.props.children){
-      if (Array.isArray(rendered.props.children)) {
-        rendered.props.children = rendered.props.children.map(renderRecursively);
-      } else {
-        rendered.props.children = [rendered.props.children].map(renderRecursively)[0];
-      }
-    }
-    return rendered;
+    this.render();
+    return this._allReactComponents;
   }
   
-  function render(componentToRender) {
-    if (DomNode.isDomNode(componentToRender)) {
+  allDomNodes() {
+    this.render();
+    return this._allDomNodes;
+  }
+  
+  render() {
+    if (this._rendered) {
+      return;
+    }
+    
+    function renderRecursively(componentToRender) {
+      const rendered = render(componentToRender);
+      if (rendered && rendered.props  && rendered.props.children){
+        if (Array.isArray(rendered.props.children)) {
+          rendered.props.children = rendered.props.children.map(renderRecursively);
+        } else {
+          rendered.props.children = [rendered.props.children].map(renderRecursively)[0];
+        }
+      }
+      return rendered;
+    }
+    
+    function render(componentToRender) {
+      if (DomNode.isDomNode(componentToRender)) {
+        return componentToRender;
+      }
+      if (ReactComponent.isReactComponent(componentToRender)) {
+        allReactComponents.push(componentToRender);
+        return ReactComponent.render(componentToRender);
+      }
       return componentToRender;
     }
-    if (ReactComponent.isReactComponent(componentToRender)) {
-      allReactComponents.push(componentToRender);
-      return ReactComponent.render(componentToRender);
-    }
-    return componentToRender;
+  
+    let allReactComponents = [];
+    this._allDomNodes = renderRecursively(this.component);
+    this._allReactComponents = allReactComponents;
+    this._rendered = true;
   }
-
-  let allReactComponents = [];
-  renderRecursively(componentToRender);
-  return allReactComponents;
 }
