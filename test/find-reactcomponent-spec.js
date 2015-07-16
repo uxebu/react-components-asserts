738b27ee0ca9554fef33341a81851542a14a40ef
Wolfram Kriesing <wk@uxebu.com>
2015-07-16 19:16:00 +0200

    Copy+pasted recursive react components extraction.

--- test/find-reactcomponent-spec.js
+++ test/find-reactcomponent-spec.js
@@ -16,17 +16,58 @@ describe('find react components', function() {
       
       assert.equal(findReactComponent(component).length, 1);
     });
-    it('two, as siblings', function() {
+    describe('two', function() {
       class Comp extends React.Component { render() { return <b/>; } }
-      const component = <a><Comp/><Comp/></a>;
-      
-      assert.equal(findReactComponent(component).length, 2);
+      it('as siblings', function() {
+        const component = <a><Comp/><Comp/></a>;
+        
+        assert.equal(findReactComponent(component).length, 2);
+      });
+      it('nested inside each other', function() {
+        class Comp extends React.Component { render() { 
+          return <b>{this.props.children}</b>; 
+        } }
+        class Inner extends React.Component { render() { return <span/>; } }
+        const component = <a><Comp><Inner/></Comp></a>;
+        
+        assert.equal(findReactComponent(component).length, 2);
+      });
     });
   });
 });
 
 const ensureToBeArray = (mayBeArray) => Array.isArray(mayBeArray) ? mayBeArray : [mayBeArray];
 function findReactComponent(component) {
-  const rendered = ReactComponent.render(component);
-  return ensureToBeArray(rendered.props.children);
+  return allReactComponents(component);
+}
+
+import DomNode from '../src/domnode.js';
+
+function allReactComponents(componentToRender) {
+  function renderRecursively(componentToRender) {
+    const rendered = render(componentToRender);
+    if (rendered && rendered.props  && rendered.props.children){
+      if (Array.isArray(rendered.props.children)) {
+        rendered.props.children = rendered.props.children.map(renderRecursively);
+      } else {
+        rendered.props.children = [rendered.props.children].map(renderRecursively)[0];
+      }
+    }
+    return rendered;
+  }
+  
+  function render(componentToRender) {
+    if (DomNode.isDomNode(componentToRender)) {
+      return componentToRender;
+    }
+    if (ReactComponent.isReactComponent(componentToRender)) {
+      allReactComponents.push(componentToRender);
+      return ReactComponent.render(componentToRender);
+    }
+    return componentToRender;
+  }
+
+  let allReactComponents = [];
+  renderRecursively(componentToRender);
+  return allReactComponents;
 }
